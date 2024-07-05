10ce56354308aea828732f0fc33b3ec885dd845e
maham-ishtiaq-2000 <mahamishtiaq2000@gmail.com>
2024-07-04 20:52:34 -0700

    mynewone

--- models/Order.js
+++ models/Order.js
@@ -4,9 +4,7 @@ const orderSchema = new mongoose.Schema({
   customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   products: [{ product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: Number }],
   status: { type: String, enum: ['pending', 'accepted', 'delivered'], default: 'pending' },
-  deliveryBoy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-  customerAddress: { type: String, required: true },
-  farmOwnerAddress: { type: String, required: true }
+  deliveryBoy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
